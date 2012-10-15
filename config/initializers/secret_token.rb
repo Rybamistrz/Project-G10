75030aa185babc3ef3c3070793853c9bc5ccdf0a
Rybamistrz <pawel.witold.okon@gmail.com>
2012-10-15 19:12:22 +0200

    Added rails app backbone

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G10Rails::Application.config.secret_token = 'ef5d14890d2c6f43316519ae4f5c45ba8e2036f55c66b533d0841706c9e5148ab69aa49e623b76ef549c85b9c91e677eb6bd183c8d0aff10c409624625f15a92'
