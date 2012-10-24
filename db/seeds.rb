415b66c698662b1c527153529da8342f7d8b8e1f
Rybamistrz <pawel.witold.okon@gmail.com>
2012-10-24 18:44:16 +0200

    created default user, display log out link only for logged in user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create email: "rybka15-15@o2.pl", password: "admin1"
