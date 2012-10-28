2ca87508ccc4f8d3b57e141808bb7e99aed20d95
Rybamistrz <pawel.witold.okon@gmail.com>
2012-10-28 11:31:38 +0100

    updated layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  layout :switch_layout
+
   before_filter :authenticate_user!
+
+  private
+
+  def switch_layout
+    unless current_user.blank?
+      "dashboard"
+    else
+      "application"
+    end
+  end
 end
